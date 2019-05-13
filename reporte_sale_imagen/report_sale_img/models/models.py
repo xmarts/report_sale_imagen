bdad2c0373528196821d6f01cea63c43fc2009d6
Juan Carlos Navarro Velazquez <navel140494@gmail.com>
2019-05-13 12:56:36 -0500

    Actualizacion de modelos.

--- reporte_sale_imagen/report_sale_img/models/models.py
+++ reporte_sale_imagen/report_sale_img/models/models.py
@@ -1,9 +1,32 @@
 # -*- coding: utf-8 -*-
+import logging
+import re
 
-from odoo import models, fields, api,_
-from odoo import tools
+from odoo import api, fields, models, tools, _
+from odoo.exceptions import ValidationError
+from odoo.osv import expression
+
+from odoo.addons import decimal_precision as dp
+
+from odoo.tools import float_compare, pycompat
 
 class report_sale_imagen(models.Model):
 	_inherit ='sale.order.line'
 
 	time_delivery = fields.Text(string = "Tiempo entrega", index=True)
+
+	image = fields.Binary(string="Imagen", related='product_id.product_tmpl_id.image_small',
+        help="Small-sized image of the product. It is automatically "
+             "resized as a 64x64px image, with aspect ratio preserved. "
+             "Use this field anywhere a small image is required.")
+
+	
+
+
+class purchase_orde_img(models.Model):
+	_inherit = 'purchase.order.line'
+
+	img_purchase = fields.Binary(string="Imagen", related='product_id.image_small',
+        help="Small-sized image of the product. It is automatically "
+             "resized as a 64x64px image, with aspect ratio preserved. "
+             "Use this field anywhere a small image is required.")
